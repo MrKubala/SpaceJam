31d07b9894cf7853bbd80570ffdc9d6efbc71ad3
Foltyn, Jakub <jakub.foltyn@sabre.com>
2017-12-08 23:18:51 +0100

    Added mechanism for synchronized font loading

--- js/main.js
+++ js/main.js
@@ -11,13 +11,14 @@ window.addEventListener("resize", function () { // Watch for browser/canvas resi
 let createScene = function () {
    // Create the scene space
    scene = new BABYLON.Scene(engine);
+   // scene.debugLayer.show();
 
    // Add lights to the scene
    let light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
    let light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
    player = new Player(1, scene);
-   map = new Map (scene);
+   map = new Map(scene);
    // Add a camera to the scene and attach it to the canvas
    let camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, player.position.z + 7, -20), scene);
 
@@ -34,11 +35,14 @@ let createScene = function () {
    return scene;
 };
 
-createScene(); //Call the createScene function
 
-engine.runRenderLoop(function () { // Register a render loop to repeatedly render the scene
-   let deltaTime = engine.getDeltaTime() / 1000;
-   player.update(deltaTime);
-   map.update(deltaTime);
-   scene.render();
-});
+function main() {
+   createScene(); //Call the createScene function
+
+   engine.runRenderLoop(function () { // Register a render loop to repeatedly render the scene
+      let deltaTime = engine.getDeltaTime() / 1000;
+      player.update(deltaTime);
+      map.update(deltaTime);
+      scene.render();
+   });
+}
