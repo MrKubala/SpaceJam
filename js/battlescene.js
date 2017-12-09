0105b72db6c5f47599b690b458747df87cb9cb22
Foltyn, Jakub <jakub.foltyn@sabre.com>
2017-12-09 14:14:32 +0100

    Added animation to station model

--- js/battlescene.js
+++ js/battlescene.js
@@ -18,7 +18,7 @@ Player = function (size, scene) {
    this.velocityz = 0;
 };
 
-Map = function (scene,scrollSpeed ,mapSize) {
+Map = function (scene, scrollSpeed, mapSize) {
    this.scrollSpeed = scrollSpeed;
    BABYLON.Mesh.call(this, "Map", scene);
    let vd = BABYLON.VertexData.CreateBox(1);
@@ -28,7 +28,7 @@ Map = function (scene,scrollSpeed ,mapSize) {
    this.scaling.z = mapSize;
    this.position.x = 0;
    this.position.y = 0;
-   this.position.z = this.scaling.z/2 - 20;
+   this.position.z = this.scaling.z / 2 - 20;
 
    // The box creation
    this.skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
@@ -48,13 +48,13 @@ Map = function (scene,scrollSpeed ,mapSize) {
 
 Asteroid = function (scene, meshInstance, mapSize, posZ) {
    BABYLON.Mesh.call(this, "Asteroid " + posZ, scene);
-   let posX = Math.floor(Math.random() * mapSize) - mapSize/2;
+   let posX = Math.floor(Math.random() * mapSize) - mapSize / 2;
    this.isAlive = true;
    this.meshInstance = meshInstance;
-   this.position = new BABYLON.Vector3(posX, this.position.y/2, posZ);
+   this.position = new BABYLON.Vector3(posX, this.position.y / 2, posZ);
    this.meshInstance.position = this.position;
 
-   this.angularSpeed={
+   this.angularSpeed = {
       x: Math.random(),
       y: Math.random(),
       z: Math.random()
@@ -63,11 +63,12 @@ Asteroid = function (scene, meshInstance, mapSize, posZ) {
 
 Food = function (scene, meshInstance, mapSize, posZ) {
    BABYLON.Mesh.call(this, "Food " + posZ, scene);
-   let posX = Math.floor(Math.random() * mapSize) - mapSize/2;
+   let posX = Math.floor(Math.random() * mapSize) - mapSize / 2;
    this.isAlive = true;
    this.meshInstance = meshInstance;
-   this.position = new BABYLON.Vector3(posX, this.position.y/2, posZ);
+   this.position = new BABYLON.Vector3(posX, this.position.y / 2, posZ);
    this.meshInstance.position = this.position;
+   this.delta = Math.random() * 100;
 };
 
 Player.prototype = Object.create(BABYLON.Mesh.prototype);
@@ -88,18 +89,18 @@ Map.prototype.update = function (deltaTime) {
 };
 Asteroid.prototype.update = function (deltaTime) {
    this.move(deltaTime);
-   if(this.position.z - player.position.z < 5 && this.meshInstance.intersectsMesh(MESH_REPO.ship, true)){
+   if (this.position.z - player.position.z < 5 && this.meshInstance.intersectsMesh(MESH_REPO.ship, true)) {
       this.dies();
    }
 };
-Asteroid.prototype.clean = function(){
-   if(this.position.z < player.position.z - 10){
+Asteroid.prototype.clean = function () {
+   if (this.position.z < player.position.z - 10) {
       this.isAlive = false;
       this.dispose();
    }
 };
-Food.prototype.clean = function(){
-   if(this.position.z < player.position.z - 10){
+Food.prototype.clean = function () {
+   if (this.position.z < player.position.z - 10) {
       this.isAlive = false;
       this.meshInstance.dispose();
       this.dispose();
@@ -107,13 +108,13 @@ Food.prototype.clean = function(){
 }
 Food.prototype.update = function (deltaTime) {
    this.move(deltaTime);
-   if(this.position.z - player.position.z < 10 && this.meshInstance.intersectsMesh(MESH_REPO.ship, false)){
+   if (this.position.z - player.position.z < 10 && this.meshInstance.intersectsMesh(MESH_REPO.ship, false)) {
       this.dies();
    }
 };
 Asteroid.prototype.move = function (deltaTime) {
-   if (this.position.z >= (-map.scaling.z)/2){
-      this.position.z -= map.scrollSpeed * deltaTime 
+   if (this.position.z >= (-map.scaling.z) / 2) {
+      this.position.z -= map.scrollSpeed * deltaTime
    }
    this.meshInstance.rotation.x += this.angularSpeed.x * deltaTime;
    this.meshInstance.rotation.y += this.angularSpeed.y * deltaTime;
@@ -122,18 +123,21 @@ Asteroid.prototype.move = function (deltaTime) {
    this.meshInstance.position.z = this.position.z;
 }
 Food.prototype.move = function (deltaTime) {
-   if (this.position.z >= (-map.scaling.z)/2){
+   this.meshInstance.rotation.y += (Math.PI / 2) * deltaTime;
+   this.delta += 2 * deltaTime;
+   this.meshInstance.position.y = Math.cos(this.delta / 2);
+   if (this.position.z >= (-map.scaling.z) / 2) {
       this.position.z -= map.scrollSpeed * deltaTime
    }
 }
-Asteroid.prototype.dies = function (){
+Asteroid.prototype.dies = function () {
    COMMONS.stats.ShipHull -= 10;
 
-   if(Math.random() * 5 < 1){
+   if (Math.random() * 5 < 1) {
       SOUNDS.wilhelScream.play();
    }
 
-   if(COMMONS.stats.ShipHull <= 0) {
+   if (COMMONS.stats.ShipHull <= 0) {
       player.isAlive = false;
       console.log("Ship is destroyed,game should end here !");
       SOUNDS.music.stop();
@@ -143,18 +147,18 @@ Asteroid.prototype.dies = function (){
       isLevelPlaying = false;
       showShipDestroyedWindow();
    }
-   console.log("Player Health count: " + COMMONS.stats.ShipHull  );
+   console.log("Player Health count: " + COMMONS.stats.ShipHull);
    this.isAlive = false;
 }
-Food.prototype.dies = function (){
+Food.prototype.dies = function () {
    COMMONS.stats.Food += 1;
-   console.log("Food count: " + COMMONS.stats.Food );
+   console.log("Food count: " + COMMONS.stats.Food);
    this.meshInstance.dispose();
    this.isAlive = false;
 }
 Map.prototype.move = function (deltaTime) {
-   if (map.position.z >= (-map.scaling.z)/2){
-      map.position.z -= this.scrollSpeed * deltaTime 
+   if (map.position.z >= (-map.scaling.z) / 2) {
+      map.position.z -= this.scrollSpeed * deltaTime
    }
 }
 
@@ -162,7 +166,7 @@ Player.prototype.move = function (deltaTime) {
    if (INPUT.d) {
 
       player.velocity += this.acceleration * deltaTime;
-     
+
    }
    if (INPUT.a) {
 
@@ -172,29 +176,30 @@ Player.prototype.move = function (deltaTime) {
    if (INPUT.space) {
       //player.position.x -= this.speed * deltaTime;
    }
-   if ( (!INPUT.d && !INPUT.a)  && Math.abs(player.velocity) < 0.01){
+   if ((!INPUT.d && !INPUT.a) && Math.abs(player.velocity) < 0.01) {
       player.velocity = 0
-   }if (player.velocity > 2){
+   }
+   if (player.velocity > 2) {
       player.velocity = 2
    }
 
-   if (player.position.x >= -(map.scaling.x/2)){
+   if (player.position.x >= -(map.scaling.x / 2)) {
       player.position.x += player.velocity;
-   }else{
-      player.position.x = -(map.scaling.x/2);
+   } else {
+      player.position.x = -(map.scaling.x / 2);
       player.velocity = 0;
    }
-   if(player.position.z >=(map.position.z + map.scaling.z/2)){
+   if (player.position.z >= (map.position.z + map.scaling.z / 2)) {
       player.velocityz += 1 * deltaTime;
       player.position.z += player.velocityz;
    }
-   if (player.position.x <= map.scaling.x/2){
+   if (player.position.x <= map.scaling.x / 2) {
       player.position.x += player.velocity;
-   }else{
-      player.position.x = map.scaling.x/2;
+   } else {
+      player.position.x = map.scaling.x / 2;
       player.velocity = 0;
    }
-   if(player.position.z >= map.scaling.z/4){
+   if (player.position.z >= map.scaling.z / 4) {
       isLevelFinished = true;
       isLevelPlaying = false;
    }
